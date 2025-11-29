6bdc9842942975c25214ebea0d10e90fb63052fa
Carl <P.Hogh@gmx.net>
2025-11-29 23:14:39 +0100

    einstellung für Lokal host

--- Program.cs
+++ Program.cs
@@ -1,22 +1,47 @@
 using Microsoft.EntityFrameworkCore;
 using MovieHall.Data;
+using Microsoft.Extensions.Hosting.WindowsServices;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+// --- Log-Ordner sicherstellen ---
+var logPath = Path.Combine(AppContext.BaseDirectory, "logs");
+if (!Directory.Exists(logPath))
+    Directory.CreateDirectory(logPath);
 
-//DB
+// --- Serilog konfigurieren ---
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .WriteTo.File(
+        path: Path.Combine(logPath, "moviehall.log"),
+        rollingInterval: RollingInterval.Day)
+    .WriteTo.Console()
+    .CreateLogger();
+
+builder.Host.UseSerilog();
+
+// --- Windows Service aktivieren ---
+builder.Host.UseWindowsService();
+
+// --- Kestrel konfigurieren ---
+builder.WebHost.UseUrls("http://localhost:5000");
+
+// --- Connection-String prüfen ---
+var connStr = builder.Configuration.GetConnectionString("DefaultConnection");
+Log.Information("Using connection string: {ConnStr}", connStr);
+
+// --- MVC + DbContext ---
+builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(connStr));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// --- HTTP Pipeline ---
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -24,11 +49,22 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-app.Run();
+try
+{
+    Log.Information("MovieHall Service is starting...");
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "MovieHall terminated unexpectedly!");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
